68c7f2653f938ceb2a8084038c65fdc8ad785623
Sri Kurniyan Sari <srikurniyans@gmail.com>
2018-05-23 18:54:22 -0700

    database sp_tht
    by sri

--- sp_tht.sql
+++ sp_tht.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2018 at 12:59 AM
+-- Generation Time: May 24, 2018 at 03:51 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -34,6 +34,29 @@ CREATE TABLE `tbl_gejala` (
   `gejala` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_gejala`
+--
+
+INSERT INTO `tbl_gejala` (`id`, `kd_gejala`, `gejala`) VALUES
+(1, 'KG1', 'Telinga Tersumbat'),
+(10, 'KG10', 'Mudah Beringus Dan Batuk'),
+(11, 'KG11', 'Selalu Pilek'),
+(12, 'KG12', 'Gangguan Pendengaran'),
+(13, 'KG13', 'Banyak Lendir Ditenggorokan'),
+(14, 'KG14', 'Pendengaran Bergemah'),
+(15, 'KG15', 'Keluar Ingus'),
+(16, 'KG16', 'Nyeri Telinga'),
+(17, 'KG17', 'Gangguan Penghidu'),
+(2, 'KG2', 'Sakit Telinga'),
+(3, 'KG3', 'Ingus Encer'),
+(4, 'KG4', 'Telinga Mendengung'),
+(5, 'KG5', 'Hidung Tersumbat'),
+(6, 'KG6', 'Keluar Cairan Pada Telinga'),
+(7, 'KG7', 'Pendengaran Menurun (Berkurang'),
+(8, 'KG8', 'Bersin-Bersin'),
+(9, 'KG9', 'Batuk-Batuk');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +68,21 @@ CREATE TABLE `tbl_penyakit` (
   `kd_penyakit` varchar(8) NOT NULL,
   `nm_penyakit` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_penyakit`
+--
+
+INSERT INTO `tbl_penyakit` (`id`, `kd_penyakit`, `nm_penyakit`) VALUES
+(1, 'KP1', 'Otitis Media Supuratif Akut'),
+(2, 'KP2', 'Laringo Faringeal Reflux'),
+(3, 'KP3', 'Otitis Externa'),
+(4, 'KP4', 'Miringitis'),
+(5, 'KP5', 'Otitis Media Efusi'),
+(6, 'KP6', 'Rhinitis Kronis Alergi'),
+(7, 'KP7', 'Rhinitis Kronis'),
+(8, 'KP8', 'Otitis Media Kronik'),
+(9, 'KP9', 'Rhinitis Akut');
 
 -- --------------------------------------------------------
 
