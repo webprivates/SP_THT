c814b06043e0b2f4cef1a0327aede1230dd014eb
Sri Kurniyan Sari <srikurniyans@gmail.com>
2018-07-11 22:36:37 +0800

    perubahan database
    by sri

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,4 +1,4 @@
- <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+  <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
